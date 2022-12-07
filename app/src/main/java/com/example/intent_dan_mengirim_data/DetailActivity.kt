5a6ac75c4ae783b528fa5859ea96502af7dea15b
zakiburnama <zaki.burnama1017@gmail.com>
2022-12-07 07:51:00 +0700

    cleaning code and add comment

--- app/src/main/java/com/example/intent_dan_mengirim_data/DetailActivity.kt
+++ app/src/main/java/com/example/intent_dan_mengirim_data/DetailActivity.kt
@@ -1,43 +1,34 @@
 package com.example.intent_dan_mengirim_data
 
-import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
-import android.widget.Toast
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.intent_dan_mengirim_data.anime.animeapi.DetailAnimeAdapter
 import com.example.intent_dan_mengirim_data.databinding.ActivityDetailBinding
-import com.loopj.android.http.AsyncHttpClient
-import com.loopj.android.http.AsyncHttpResponseHandler
-import com.squareup.picasso.Picasso
-import cz.msebera.android.httpclient.Header
-import org.json.JSONArray
-import org.json.JSONObject
 
+@Suppress("DEPRECATION")
 class DetailActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityDetailBinding
-    private lateinit var getAnime: String
-    private val listFacts = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityDetailBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_detail)
 
         val tvGetText: TextView = findViewById(R.id.tv_text1)
 
+        // Mengambil nilai yang dikirmkan dari MainActivity
+        // Karena nilai berupa string maka .getStringExtra
         val getText = intent?.getStringExtra(EXTRA_TEXT)
+
         tvGetText.text = getText
 
+        // Mengambil nilai yang dikirmkan dari MainActivity
+        // Karena nilai yang dikirim berupa bolean maka .getBooleanExtra
         val getBool = intent?.getBooleanExtra(EXTRA_BOOL, false)
 
         if (getBool == true) {
+            // Mengambil nilai yang dikirmkan dari MainActivity
+            // Karena nilai yang dikirim berupa objek dengan data class menggunakan Parcelabel maka kodenya sbb
             val getCar = intent?.getParcelableExtra<Car>(EXTRA_CAR)
-            Log.i("TAG", getCar?.name.toString())
             val carSpek = "Car Specification:\n" +
                     "\nName: ${getCar?.name.toString()} " +
                     "\nBrand: ${getCar?.brand.toString()} " +
@@ -47,62 +38,12 @@ class DetailActivity : AppCompatActivity() {
             tvGetText.text = carSpek
         }
 
-
-        // Untuk NETWORKING
-        getAnime = intent?.getStringExtra(EXTRA_ANIME).toString()
-
-        val getAnimeImg = intent?.getStringExtra(EXTRA_ANIME_IMG).toString()
-        Picasso.get().load(getAnimeImg).into(binding.ivAnime)
-
-        getListFacts()
     }
 
-    private fun getListFacts() {
-        val client = AsyncHttpClient()
-        val url = "https://anime-facts-rest-api.herokuapp.com/api/v1/"+getAnime
-        client.get(url, object : AsyncHttpResponseHandler() {
-            override fun onSuccess(statusCode: Int, headers: Array<Header>,
-                                   responseBody: ByteArray) {
-                val result = String(responseBody)
-                try {
-                    val responseObject = JSONObject(result)
-                    val data = responseObject.getString("data")
-                    val jsonArray = JSONArray(data)
-                    for (i in 0 until jsonArray.length()) {
-                        val jsonObject = jsonArray.getJSONObject(i)
-                        val fact = jsonObject.getString("fact")
-                        listFacts.add(fact)
-                    }
-                    showRecycler()
-                } catch (e: Exception) {
-                    Toast.makeText(this@DetailActivity, e.message, Toast.LENGTH_SHORT).show()
-                }
-            }
-            override fun onFailure(statusCode: Int, headers: Array<Header>,
-                                   responseBody: ByteArray, error: Throwable) {
-                Toast.makeText(this@DetailActivity, statusCode, Toast.LENGTH_SHORT).show()
-            }
-        })
-    }
-
-    private fun showRecycler() {
-        if (applicationContext.resources.configuration.orientation == Configuration
-                .ORIENTATION_LANDSCAPE) {
-            binding.listAnime.layoutManager = GridLayoutManager(this, 2)
-        } else {
-            binding.listAnime.layoutManager = LinearLayoutManager(this)
-        }
-
-        val adapter = DetailAnimeAdapter(listFacts)
-        binding.listAnime.adapter = adapter
-    }
-
-
+    // Variabel dibawah yang digunakan pada MainActivity tadi untuk mengrimkan nilai kesini
     companion object {
         const val EXTRA_TEXT = "extra_text"
         const val EXTRA_BOOL = "extra_bool"
         const val EXTRA_CAR = "extra_car"
-        const val EXTRA_ANIME = "extra_anime"
-        const val EXTRA_ANIME_IMG = "extra_anime_img"
     }
 }
--- app/src/main/java/com/example/intent_dan_mengirim_data/MainActivity.kt
+++ app/src/main/java/com/example/intent_dan_mengirim_data/MainActivity.kt
@@ -7,37 +7,51 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.EditText
-import com.example.intent_dan_mengirim_data.anime.ListAnimeActivity
 
 class MainActivity : AppCompatActivity() {
 
+    // Deklarasikan variabel global etKirimData dengan tipe EditText yang bakal menampung view EditText
     lateinit var etKirimData: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Deklarasikan variabel etKirimData tapi untuk dihubungkan dengan view dengan id et_kirim_data
         etKirimData = findViewById(R.id.et_kirim_data)
 
     }
 
+    // Fungsi onClick untuk menangkap interaksi 'click' pada button yang telah dideklarasikan bahwa button tersebut menggunakan fungsi onlClick
     fun onClick(view: View) {
+        // Percabangan karena banyak button yang bisa ditekan, maka dibuat percabanagan dengan parameter id
+        // Jadi Perintah berjalan sesuai dengan id button yang ditekan
         when (view.id) {
+            // Untuk melakukan Intent explicit biasa, memulai activity baru
             R.id.btn_intent_explicit -> {
-//                val intent = Intent(this@MainActivity, DetailActivity::class.java)
-                val intent = Intent(this@MainActivity, ListAnimeActivity::class.java)
-                startActivity(intent)
-            }
-
-            R.id.btn_intent_data -> {
-                val text = etKirimData.text.toString()
+                // Berikut cara deklarasikan Intent
+                // Intent('ASAL', 'TUJUAN')
                 val intent = Intent(this@MainActivity, DetailActivity::class.java)
-                intent.putExtra(DetailActivity.EXTRA_TEXT, text)
-//                val intent = Intent(this@MainActivity, HomeworkActivity::class.java)
+                // Mulai activity
                 startActivity(intent)
             }
 
+            // Melakukan intent dengan mengirimkan nilai ke Activity tujuan
+            R.id.btn_intent_data -> {
+                // Mengambil value pada edit text, lalu disimpan pada variabel text dalam bentuk string
+                val text = etKirimData.text.toString()
+                // Deklarasi Intent
+                val intent = Intent(this@MainActivity, DetailActivity::class.java)
+                // Mengirimkan text yang disimpan tadi ke Activity tujuan dengan cara
+                // intent.putExtra('TUJUAN'.'NAMA VARIABEL PENERIMA', 'NILAI YANG DIKRIM')
+                intent.putExtra(DetailActivity.EXTRA_TEXT, text)
+                // Mulai Activity
+                startActivity(intent)
+            }
+
+            // Melalukan intent denganmengirimkan objek ke Activity tujuan
             R.id.btn_intent_objek -> {
+                // Buat variabel untuk menampil nilai dengan tipe objek car (sudah dibuat data class-nya car.kt)
                 val carSpek = Car(
                     "Civic",
                     "Honda",
@@ -45,13 +59,17 @@ class MainActivity : AppCompatActivity() {
                     1997,
                     57000000.00
                 )
-                Log.i("TAG", carSpek.toString())
+                // Intent
                 val intent = Intent(this@MainActivity, DetailActivity::class.java)
+                // Mengirimkan objek car, caranya sama saja dengan mengiriman nilai
                 intent.putExtra(DetailActivity.EXTRA_CAR, carSpek)
+                // Mengirimkan nilai true ke tujuan
                 intent.putExtra(DetailActivity.EXTRA_BOOL, true)
+                // Mulai Activity
                 startActivity(intent)
             }
 
+            // Contoh Intent Implicit
             R.id.btn_intent_implicit -> {
                 val phoneNumber = "081234567890"
                 val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
